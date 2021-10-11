5491e01ff576c5259c98e4b039f8e76d6cdadfd8
Yussuf Adan Ali <55383633+usfali-ke@users.noreply.github.com>
2021-10-11 20:25:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def hello_world():
     return "<p>Hello, World! This is an Updated site!!</p>"
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=9000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
     #app.run(host='0.0.0.0', port=9000)
